d76403afe79835665a4bc59c99ba4af06812d509
Kaitlyn Hova <kaitlynhova@gmail.com>
2019-07-21 15:21:16 -0700

    Creating theme.js

---
+++ src/theme.js
@@ -0,0 +1,44 @@
+export const theme = {
+    backgroundColors: {
+        button: "#F82636",
+        grid: "#583637",
+        screen: "#2F689F",
+    },
+    boxShadow: {
+        button: {
+            active: "0 0 #A8000B",
+            base: "0 8px #A8000B",
+        },
+        square: {
+            active: "0 0 10px #FFFFFF",
+        }
+    },
+    breakpoints: {
+        s: 678,
+    },
+    fontColors: {
+        p: "#FFFFFF",
+    },
+    fontSizes: {
+        p: 20,
+    },
+    grid: {
+        gap: {
+            s: 10,
+            l: 16,
+        }
+    },
+    // red, orange, yellow, green, blue, purple, gray, black
+    jewelColors: ["#F82636", "#EC8C17", "#EFFA1E", "#38D020", "#13A0E5", "#B401AF", "#F5FCF7", "#30373A"],
+    spacing: {
+        none: 0,
+        xs: 4,
+        s: 8,
+        m: 16,
+        l: 32,
+        xl: 64
+    },
+    widths: {
+        screen: 800,
+    }
+}
